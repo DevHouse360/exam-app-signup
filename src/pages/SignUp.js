75882b0ff9fb719e7d9023c07d9f7a842fca3657
Stephen Osei-Bonsu <stephen.bonsu@accertix.com>
2021-10-21 22:29:16 +0000

    [ADDED] form validation

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
 
@@ -13,50 +13,88 @@ axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
 const SignUp = ({ handleSuccessResponse, handleErrorResponse }) => {
-  const handleFormSubmit = async (values) => {
-    try {
-      const data = {
-        organization_name: values?.organization_name,
-        given_name: values?.given_name,
-        family_name: values?.family_name,
-        email: values?.email,
-        password: values?.password,
-      };
-      const result = await axios.post(signUp_url, data);
-      const response = result.data;
-      // Passing response out to parent component!!
-      handleSuccessResponse(response);
-      console.log(response);
-    } catch (error) {
-      handleErrorResponse(error);
-      console.log(error);
-    }
+  const initialFormValues = {
+    organization_name: "",
+    given_name: "",
+    family_name: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+    isShowPassword: false,
+  };
+  const [formValues, setformValues] = useState(initialFormValues);
+  const [formErrors, setformErrors] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleFormChange = (e) => {
+    const { value, type, checked, name } = e.target;
+    type === "checkbox"
+      ? setformValues({ ...formValues, [name]: checked })
+      : setformValues({ ...formValues, [name]: value });
   };
 
   const formValidation = (values) => {
     const errors = {};
-    if (!values.organization_name) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+    if (!values.organization_name.trim()) {
       errors.organization_name = "name of organization is required";
     }
-    if (!values.given_name) {
+    if (!values.given_name.trim()) {
       errors.given_name = "first name is required";
     }
-    if (!values.family_name) {
+    if (!values.family_name.trim()) {
       errors.family_name = "last name is required";
     }
-    if (!values.email) {
+    if (!values.email.trim()) {
       errors.email = "email is required";
+    } else if (!emailRegex.test(values.email.trim())) {
+      errors.email = "this is not a valid email";
     }
 
-    if (!values.password) {
+    if (!values.password.trim()) {
       errors.password = "password is required";
     }
-    if (!values.repeat) {
-      errors.repeat = "password confirmation is required";
+    if (!values.repeatPassword.trim()) {
+      errors.repeatPassword = "password confirmation is required";
+    } else if (values.password !== values.repeatPassword) {
+      errors.repeatPassword = "passwords do not match";
     }
 
     return errors;
   };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    setformErrors(formValidation(formValues));
+    setIsSubmitted(true);
+  };
+
+  useEffect(() => {
+    const submitForm = async () => {
+      try {
+        const data = {
+          organization_name: formValues?.organization_name,
+          given_name: formValues?.given_name,
+          family_name: formValues?.family_name,
+          email: formValues?.email,
+          password: formValues?.password,
+        };
+        const result = await axios.post(signUp_url, data);
+        const response = result.data;
+        // Passing response out to parent component!!
+        handleSuccessResponse(response);
+        console.log(response);
+      } catch (error) {
+        handleErrorResponse(error);
+        console.log(error);
+      }
+    };
+
+    if (Object.keys(formErrors).length === 0 && isSubmitted) {
+      submitForm();
+    }
+  }, [formErrors]);
 
   return (
     <Container fluid>
@@ -87,22 +125,46 @@ const SignUp = ({ handleSuccessResponse, handleErrorResponse }) => {
                   <Col>
                     <Form.Group>
                       <Form.Label>Organization Name</Form.Label>
-                      <Form.Control type='text' name='organization_name' />
+                      <Form.Control
+                        type='text'
+                        name='organization_name'
+                        value={formValues.organization_name}
+                        placeholder='name of your organization'
+                        onChange={handleFormChange}
+                      />
+                      <small className='text-danger'>
+                        {formErrors.organization_name}
+                      </small>
                     </Form.Group>
-                    <small className='text-danger'>{}</small>
                   </Col>
                 </Row>
                 <Row className='mb-3'>
                   <Col>
                     <Form.Group>
                       <Form.Label>First Name</Form.Label>
-                      <Form.Control type='text' name='given_name' />
+                      <Form.Control
+                        type='text'
+                        name='given_name'
+                        onChange={handleFormChange}
+                        value={formValues.given_name}
+                      />
+                      <small className='text-danger'>
+                        {formErrors.given_name}
+                      </small>
                     </Form.Group>
                   </Col>
                   <Col>
                     <Form.Group>
                       <Form.Label>Last Name</Form.Label>
-                      <Form.Control type='text' name='family_name' />
+                      <Form.Control
+                        type='text'
+                        name='family_name'
+                        onChange={handleFormChange}
+                        value={formValues.family_name}
+                      />
+                      <small className='text-danger'>
+                        {formErrors.family_name}
+                      </small>
                     </Form.Group>
                   </Col>
                 </Row>
@@ -110,7 +172,13 @@ const SignUp = ({ handleSuccessResponse, handleErrorResponse }) => {
                   <Col>
                     <Form.Group>
                       <Form.Label>email</Form.Label>
-                      <Form.Control type='email' name='email' />
+                      <Form.Control
+                        type='email'
+                        name='email'
+                        onChange={handleFormChange}
+                        value={formValues.email}
+                      />
+                      <small className='text-danger'>{formErrors.email}</small>
                     </Form.Group>
                   </Col>
                 </Row>
@@ -118,13 +186,42 @@ const SignUp = ({ handleSuccessResponse, handleErrorResponse }) => {
                   <Col>
                     <Form.Group>
                       <Form.Label>password</Form.Label>
-                      <Form.Control type='password' name='password' />
+                      <Form.Control
+                        type={formValues.isShowPassword ? "text" : "password"}
+                        name='password'
+                        onChange={handleFormChange}
+                        value={formValues.password}
+                      />
+                      <small className='text-danger'>
+                        {formErrors.password}
+                      </small>
                     </Form.Group>
                   </Col>
                   <Col>
                     <Form.Group>
                       <Form.Label>repeat password</Form.Label>
-                      <Form.Control type='password' name='repeatPassword' />
+                      <Form.Control
+                        type={formValues.isShowPassword ? "text" : "password"}
+                        name='repeatPassword'
+                        onChange={handleFormChange}
+                        value={formValues.repeatPassword}
+                      />
+                      <small className='text-danger'>
+                        {formErrors.repeatPassword}
+                      </small>
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row className='mb-3'>
+                  <Col>
+                    <Form.Group>
+                      <Form.Check
+                        type='checkbox'
+                        label='show password'
+                        name='isShowPassword'
+                        value={formValues.isShowPassword}
+                        onChange={handleFormChange}
+                      />
                     </Form.Group>
                   </Col>
                 </Row>
